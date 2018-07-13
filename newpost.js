0d3db856d66f04cb7039d4b85ff281bfa56d9265
Dwight Doane <dwightdoane@me.com>
2018-07-12 20:45:38 -0400

    update newpost script to handle punctation

--- newpost.js
+++ newpost.js
@@ -1,21 +1,19 @@
 const fs = require('fs');
 
 const d = new Date();
-
-// const month = d.Month();
 const month = d => {
   const m = d.getMonth() + 1;
   return m < 10 ? `0${m}` : m;
-}
+};
 
-const slug = process.argv[2].toLocaleLowerCase().split(' ').join('-');
-const date = `${d.getFullYear()}-${month(d)}-${d.getDate()}`
-const fileName = `${date}-${slug}`;
-console.log('n', fileName)
+const postName = process.argv[2];
+const slug = postName.toLocaleLowerCase().replace(/[^\w\s]/gi, '').split(' ').join('-');
+const datePrefix = `${d.getFullYear()}-${month(d)}-${d.getDate()}`
+const fileName = `${datePrefix}-${slug}`;
 const tmplt =
-`---
+  `---
 layout: post
-title:  "${process.argv[2]}"
+title:  "${postName}"
 ---
-`
-fs.writeFileSync(`${__dirname}/_posts/${fileName}.markdown`, tmplt)
+`;
+fs.writeFileSync(`${__dirname}/_posts/${fileName}.markdown`, tmplt);
